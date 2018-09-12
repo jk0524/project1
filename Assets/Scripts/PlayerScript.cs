4f26b36b406567f9193c4c571772cbe23d6c0fc3
jasonkang <kangsiliang@berkeley.edu>
2018-09-11 18:18:39 -0700

    basic functions done

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Player controller and behavior
+/// </summary>
+public class PlayerScript : MonoBehaviour
+{
+    /// <summary>
+    /// 1 - The speed of the ship
+    /// </summary>
+    public Vector2 speed = new Vector2(50, 50);
+
+    // 2 - Store the movement and the component
+    private Vector3 movement;
+
+    void Update()
+    {
+        // 3 - Retrieve axis information
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        // 4 - Movement per direction
+        movement = new Vector3(
+          speed.x * inputX,
+          speed.y * inputY,
+          0);
+
+        movement *= Time.deltaTime;
+
+        transform.Translate(movement);
+
+        bool shoot = Input.GetButtonDown("Fire1");
+        shoot |= Input.GetButtonDown("Fire2");
+
+        if (shoot) {
+
+            WeaponScript weapon = GetComponent<WeaponScript>();
+            if (weapon != null) {
+                weapon.Attack(false);
+            }
+        }
+
+    }
+
+}
